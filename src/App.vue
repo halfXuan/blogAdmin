4ca183e77a842582c30584daa9007f8a993a7b85
luoyong518 <471826078@qq.com>
2020-06-01 17:52:41 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: 471826078@qq.com
  * @Date: 2020-05-21 09:50:47
  * @LastEditors: 471826078@qq.com
- * @LastEditTime: 2020-05-21 13:57:06
+ * @LastEditTime: 2020-06-01 16:31:27
 --> 
 <template>
   <div id="app">
@@ -15,11 +15,12 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
   color: #2c3e50;
-  position: fixed;
-  width: 100%;
-  height: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   overflow: auto;
 }
 
