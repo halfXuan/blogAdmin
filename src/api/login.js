0ea67be94089d010f97de1b9b09c4c10f1912156
luoyong518 <471826078@qq.com>
2020-06-18 11:27:40 +0800

    1

--- src/api/login.js
+++ src/api/login.js
@@ -1,10 +1,10 @@
-/*
- * @Author: 471826078@qq.com
- * @Date: 2020-05-25 17:44:02
- * @LastEditors: 471826078@qq.com
- * @LastEditTime: 2020-05-26 14:57:35
+/**
+ * @name: 登录api配置
+ * @param {type} 
+ * @Author: luoyong
  */
+
 import { post } from './axios'
 
-export const apiRegister = p=>post('api/users/register',p)
-export const apiLogin = p =>post('api/users/adminLogin',p)
+export const apiRegister = p => post('api/users/register', p)
+export const apiLogin = p => post('api/users/adminLogin', p)
